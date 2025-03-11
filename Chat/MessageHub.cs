ad059b19523d91f2742ecd993c332871f13ea05f
lybachpha <bachien0987@gmail.com>
2025-03-11 13:40:46 +0700

    messages

---
+++ Chat/MessageHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace ProjectComp1640.Chat
+{
+    public class MessageHub:Hub    
+    {
+        public async Task SendMessage(string senderId, string receiverId, string content)
+        {
+            await Clients.User(receiverId).SendAsync("ReceiveMessage", senderId, content);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.User?.Identity?.Name;
+            if (userId != null)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userId = Context.User?.Identity?.Name;
+            if (userId != null)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, userId);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
