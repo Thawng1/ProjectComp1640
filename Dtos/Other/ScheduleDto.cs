559961ced8af0af6eb74d78dcd99e1a8cc39fd89
m1nkuzu <minhndgdh190046@fpt.edu.vn>
2025-03-31 14:52:43 +0700

    add LinkMeeting on Model

--- Dtos/Other/ScheduleDto.cs
+++ Dtos/Other/ScheduleDto.cs
@@ -4,6 +4,7 @@
     {
         public DayOfWeek Day { get; set; }
         public string Slot { get; set; }
+        public string LinkMeeting {  get; set; }
         public int? ClassId { get; set; }
         public int? ClassroomId { get; set; }
     }
