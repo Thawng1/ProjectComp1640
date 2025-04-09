8c0f73ccdb996d12ed98846c6ab3518e01a163f7
Mink <minhndgdh190046@fpt.edu.vn>
2025-04-09 09:25:19 +0700

    add Id to GetSubjectDto

--- Dtos/Subject/GetSubjectDto.cs
+++ Dtos/Subject/GetSubjectDto.cs
@@ -4,6 +4,7 @@ namespace ProjectComp1640.Dtos.Subject
 {
     public class GetSubjectDto
     {
+        public int Id { get; set; }
         public string SubjectName { get; set; }
         public string Information { get; set; }
         public List<CreateClassDto> Classes { get; set; } = new List<CreateClassDto>();
